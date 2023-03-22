54544e4471e7b1413c5881ee48f6f7c06afcdf06
Tiago Vasconcelos <talvasconcelos@gmail.com>
2023-03-22 10:30:03 +0000

    initial commit - WIP

---
+++ src/App.svelte
@@ -0,0 +1,319 @@
+<script>
+  import { parse } from "papaparse";
+  import { onMount } from "svelte";
+  import { poolPublish } from "./lib/nostr";
+  import { processSubstackData } from "./lib/process";
+
+  let uploadedFiles, uploadedCSV;
+  let posts, postsCSV, hasNIP07, loading;
+
+  let uploadToNostr = [];
+  let selectAll = false;
+
+  $: selectAll = posts && uploadToNostr.length == posts.length;
+
+  let platforms = [
+    { value: "substack", disabled: false },
+    { value: "ghost", disabled: true },
+    { value: "wordpress", disabled: true }
+  ];
+
+  let selectedPlatform;
+
+  onMount(async () => {
+    await timeout(500);
+    hasNIP07 = Boolean(window.nostr);
+    console.log(hasNIP07);
+    if (!hasNIP07) return;
+  });
+
+  function reset() {
+    uploadedFiles = null;
+    uploadedCSV = null;
+    posts = null;
+    postsCSV = null;
+    uploadToNostr = [];
+    selectAll = false;
+    loading = false;
+  }
+
+  function timeout(ms) {
+    return new Promise((res) => setTimeout(res, ms));
+  }
+
+  function handleSelectAll() {
+    // some hack to handle checked
+    if (!selectAll) {
+      uploadToNostr = posts;
+    } else if (selectAll) {
+      uploadToNostr = [];
+    }
+  }
+
+  async function publish() {
+    if (!hasNIP07) return;
+
+    loading = true;
+    let pubkey = await window.nostr.getPublicKey();
+
+    uploadToNostr.forEach(async (p) => {
+      let { id, title, summary, image, markdown } = p;
+
+      const tags = [["d", id]];
+
+      if (title && title !== "") tags.push(["title", title]);
+      if (summary && summary !== "") tags.push(["summary", summary]);
+      if (image && image !== "") tags.push(["image", image]);
+
+      const event = {
+        kind: 30023,
+        pubkey,
+        content: markdown,
+        created_at: Math.floor(Date.now() / 1000),
+        tags
+      };
+
+      let signed;
+      try {
+        signed = await window.nostr.signEvent(event);
+      } catch (error) {
+        loading = false;
+        console.error("failed to sign with extension", error);
+        return;
+      }
+      await poolPublish(signed);
+      await timeout(500);
+    });
+    return reset();
+  }
+
+  const upload = async () => {
+    // Get posts info from CSV
+    let csvToJSON = new Promise(function (resolve, reject) {
+      parse(uploadedCSV.item(0), {
+        header: true,
+        complete: function (response) {
+          resolve(response.data);
+        },
+        error: function (err) {
+          reject(err);
+        }
+      });
+    });
+    postsCSV = await Promise.resolve(csvToJSON);
+
+    // Convert the FileList into an array and iterate
+    let files = Array.from(uploadedFiles)
+      .map((file) => {
+        let post = postsCSV.find(
+          (p) => p.post_id == file.name.replace(".html", "")
+        );
+        console.log(post);
+        if (!post || post.is_published === "false") return;
+        let output = {};
+        output.id = post.post_id;
+        output.title = post.title;
+        output.slug = post.post_id.split(".")[1];
+        output.summary = post.subtitle;
+        // Define a new file reader
+        let reader = new FileReader();
+
+        // Create a new promise
+        return new Promise((resolve) => {
+          // Resolve the promise after reading file
+          reader.onload = () => {
+            output.html = reader.result;
+            return resolve(output);
+          };
+
+          // Read the file as a text
+          reader.readAsText(file);
+        });
+      })
+      .filter((v) => v);
+
+    // At this point you'll have an array of results
+    let res = await Promise.all(files);
+
+    posts = res.map(processSubstackData);
+    uploadedCSV = null;
+    uploadedFiles = null;
+  };
+</script>
+
+<div class="header">
+  <hgroup>
+    <h1>Migrate to Nostr</h1>
+    <h3>
+      Migrate blog posts to the
+      <a
+        href="https://github.com/nostr-protocol/nips/blob/master/23.md"
+        target="_blank"
+        rel="noopener noreferrer">nip23</a
+      > - article format for Nostr
+    </h3>
+  </hgroup>
+  <p>
+    For now it only works for Substack. You need to create an export of your
+    Substack posts, extract the <code>.zip</code> file, and upload the
+    <code>export/posts.csv</code>
+    file and the <code>.html</code> files in <code>export/posts/</code> folder.
+  </p>
+  <p>
+    The tool will convert the files to markdown and let you select which ones
+    you want to upload to Nostr.
+  </p>
+  {#if !hasNIP07}
+    <small
+      >You need to have a signing browser extension (<a
+        href="https://github.com/nostr-protocol/nips/blob/master/07.md#nip-07"
+        target="_blank"
+        rel="noopener noreferrer">NIP07</a
+      >) to use this tool</small
+    >
+  {/if}
+</div>
+<section class="grid">
+  <fieldset>
+    <label for="platform">Platform</label>
+    <select
+      id="platform"
+      bind:value={selectedPlatform}
+      required
+    >
+      {#each platforms as platform}
+        <option
+          disabled={platform.disabled}
+          value={platform.value}>{platform.value}</option
+        >
+      {/each}
+      <option disabled>...more to come</option>
+    </select>
+  </fieldset>
+</section>
+{#if selectedPlatform == "substack"}
+  <section class="grid">
+    <label for="postscsv">
+      Upload <code>posts.csv</code>
+      <input
+        bind:files={uploadedCSV}
+        type="file"
+        accept=".csv"
+      />
+    </label>
+
+    <label for="posthtml">
+      Upload <code>.html</code> files
+      <input
+        bind:files={uploadedFiles}
+        type="file"
+        accept=".html"
+        multiple
+        disabled={!uploadedCSV}
+      />
+    </label>
+  </section>
+  {#if uploadedCSV && uploadedFiles}
+    <section class="action">
+      <button
+        style="width: auto;"
+        on:click={upload}>Submit</button
+      >
+    </section>
+  {/if}
+{/if}
+
+{#if posts}
+  <section>
+    <details>
+      <summary>Posts</summary>
+      <ul>
+        <li>
+          <label for="switch">
+            <input
+              type="checkbox"
+              id="switch"
+              name="switch"
+              role="switch"
+              on:change={handleSelectAll}
+              bind:checked={selectAll}
+            />
+            Select all posts
+          </label>
+        </li>
+        {#each posts as post}
+          <li>
+            <label for="switch">
+              <input
+                type="checkbox"
+                id="switch"
+                name="switch"
+                role="switch"
+                bind:group={uploadToNostr}
+                value={post}
+              />
+              Publish - {post.title}
+            </label>
+          </li>
+        {/each}
+      </ul>
+    </details>
+    <details>
+      <summary>Upload to Nostr</summary>
+      <div>
+        <!-- <fieldset>
+          <label for="nsec"
+            >Private Key (leave blank if you have a Nostr signing extension)</label
+          >
+          <input
+            type="text"
+            placeholder="nsec/hex"
+            bind:value={sk}
+          />
+          <small>This is used to publish your posts, it won't be stored.</small>
+        </fieldset> -->
+        <button
+          disabled={uploadToNostr.length < 1}
+          style="width: auto; margin: 0 auto"
+          aria-busy={loading}
+          on:click={publish}>Button</button
+        >
+      </div>
+    </details>
+  </section>
+{/if}
+
+<style>
+  .header {
+    margin-bottom: 3rem;
+  }
+  .header h1 {
+    margin: 0;
+  }
+
+  .header p {
+    font-size: 0.9rem;
+  }
+
+  #plaform {
+    text-transform: capitalize;
+  }
+
+  code {
+    font-size: 0.75em;
+  }
+  .action {
+    display: flex;
+    justify-content: center;
+    margin-top: 3rem;
+  }
+
+  ul li {
+    list-style: none;
+    padding: 0.75rem;
+    border-bottom: 1px solid var(--muted-color);
+  }
+  li:last-child {
+    border-bottom: none;
+  }
+</style>
---
+++ src/lib/process.js
@@ -0,0 +1,162 @@
+import * as cheerio from "cheerio";
+import { parseSrcset } from "srcset";
+import TurndownService from "turndown";
+
+const turndownService = new TurndownService();
+
+const getUnsizedImageName = (str) => {
+  const noSizeRegex = /(.*)(_[0-9]{1,4}x[0-9]{1,4}.[a-z]{2,4})/gim;
+  let srcParts = str.split(/\/|%2F/);
+  let last = srcParts.slice(-1)[0];
+  let matches = noSizeRegex.exec(last);
+
+  if (matches) {
+    return matches[1];
+  } else {
+    return str;
+  }
+};
+
+const largestSrc = ($imageElem) => {
+  const src = $imageElem.attr("src");
+  const srcset = $imageElem.attr("srcset") || false;
+
+  let srcToUse = src;
+
+  if (srcset) {
+    const parsedSrcset = parseSrcset(srcset);
+
+    if (parsedSrcset && parsedSrcset.length > 0) {
+      srcToUse = parsedSrcset[0].url;
+    }
+  }
+
+  // Remove the width from the image URL
+  if (srcToUse) {
+    srcToUse = srcToUse.replace(/w_[0-9]{2,4},c_limit,/, "");
+  } else {
+    srcToUse = src;
+  }
+
+  return srcToUse;
+};
+
+function processSubstackData(data) {
+  let output = {};
+  let { html } = data;
+
+  // If there's no HTML, exit & return an empty string to avoid errors
+  if (!html) {
+    console.debug(`Post has no HTML content`);
+    return "";
+  }
+
+  // As there is HTML, pass it to Cheerio inside a `<body>` tag so we have a global wrapper to target later on
+  const $ = cheerio.load(`<body>${html}</body>`, {
+    decodeEntities: false
+  });
+
+  // Empty text elements are commonplace and are not needed
+  $("p").each((i, el) => {
+    let content = $(el).html().trim();
+
+    if (content.length === 0) {
+      $(el).remove();
+    }
+  });
+
+  let firstElement = $("body *").first();
+
+  if (
+    firstElement.tagName === "img" ||
+    ($(firstElement).get(0) && $(firstElement).get(0).name === "img") ||
+    $(firstElement).find("img").length
+  ) {
+    let theElementItself =
+      firstElement.tagName === "img" || $(firstElement).get(0).name === "img"
+        ? firstElement
+        : $(firstElement).find("img");
+    let firstImgSrc = $(theElementItself).attr("src");
+
+    if (firstImgSrc.length > 0) {
+      output.image = firstImgSrc;
+      $(firstElement).remove();
+    }
+  }
+
+  $("div.tweet").each((i, el) => {
+    let src = $(el).children("a").attr("href");
+    let url = new URL(src);
+
+    if (url.searchParams) {
+      // remove possible query params
+      url.searchParams.forEach((p) => url.searchParams.delete(p));
+    }
+    src = url.toString();
+
+    let $figure = $("<figure></figure>");
+    let $blockquote = $('<blockquote class="twitter-tweet"></blockquote>');
+    let $anchor = $(`<a href="${src}"></a>`);
+    let $script = $(
+      '<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'
+    );
+
+    $blockquote.append($anchor);
+
+    $figure.append($blockquote);
+    $figure.append($script);
+
+    $(el).replaceWith($figure);
+  });
+
+  $(".captioned-image-container").each((i, div) => {
+    const imgAlt = $(div).find("img[alt]").attr("alt") || "";
+    // const linkHref = $(div).find("a.image-link").attr("href") || false;
+    const imgCaption = $(div).find("figcaption").html() || false;
+    const imageSrc = largestSrc($(div).find("img"));
+
+    let $figure = $("<figure></figure>");
+    let $img = $(`<img src="${imageSrc}" alt="${imgAlt}">`);
+    let $caption = $(`<figcaption>${imgCaption}</figcaption>`);
+    //let $anchor = $(`<a href="${imageSrc}"></a>`);
+
+    $figure.append($img);
+    if (imgCaption) {
+      $figure.append($caption);
+    }
+
+    $(div).replaceWith($figure);
+  });
+
+  $(".image-link").each((i, anchor) => {
+    const imgAlt = $(anchor).find("img[alt]").attr("alt") || "";
+    const linkHref = $(anchor).attr("href");
+    const imageSrc = largestSrc($(anchor).find("img"));
+
+    let $anchor = $(`<a href="${linkHref}"></a>`);
+    let $img = $(`<img src="${imageSrc}" alt="${imgAlt}">`);
+
+    $anchor.append($img);
+
+    $(anchor).replaceWith(linkHref ? $anchor : $img);
+
+    $("a > style").each((i, style) => {
+      $(style).remove();
+    });
+  });
+
+  // Remove Substack buttons
+  $("p.button-wrapper").each((i, button) => {
+    $(button).remove();
+  });
+
+  // convert HTML back to a string
+  html = $("body").html();
+
+  // Apply our new HTML back to the post object
+  output.html = html.trim();
+  output.markdown = turndownService.turndown(output.html);
+  return { ...data, ...output };
+}
+
+export { processSubstackData };
